11dec7540aee45bb3929dc81e8caf845e70db5d7
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-27 00:40:33 +0545

    Ecommerce

--- e-frontend/src/Component/Navigation.jsx
+++ e-frontend/src/Component/Navigation.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { TfiAlignLeft } from "react-icons/tfi";
 import { TfiAngleDown } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import { CiDeliveryTruck } from "react-icons/ci";
+import "../index.css";
+import SlideCategory from "./SideCategory";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openSidePanel = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="navigation w-[100%]">
-      <div className="container flex items-center justify-end gap-3">
+    <div className="navigation w-[100%] nav-bar">
+      <div className="container flex items-center justify-end gap-3 wrapper-nav">
         <div className="col-1 w-[25%]">
-          <Button className="!text-black text-[30px]">
-            <TfiAlignLeft className="!mr-3" />
-            SHORT BY CATEGORIES
-            <TfiAngleDown className="!ml-3" />
-          </Button>
+          <Link to="category">
+            <Button className="!text-black text-[30px]" onClick={openSidePanel}>
+              <TfiAlignLeft className="!mr-3" />
+              SHORT BY CATEGORIES
+              <TfiAngleDown className="!ml-3" />
+            </Button>
+          </Link>
         </div>
         <div className="col-2 w-[65%]">
           <ul className="flex">
@@ -50,14 +58,14 @@ const Navigation = () => {
           </ul>
         </div>
         <div className="col-3 w-[15%] flex justify-end  ">
-            <CiDeliveryTruck />
-          <p className="text-[14px] text-[500] ml-2">
-            
-            Free Delivery 
-            
+          <CiDeliveryTruck />
+          <p className="text-[14px] text-[500] ml-2 text-red-800 hover:text-red-950 ">
+            Free Delivery
           </p>
         </div>
       </div>
+      {/* side panel props */}
+      <SlideCategory openSidePanel={openSidePanel} isOpen={isOpen}/>
     </div>
   );
 };
