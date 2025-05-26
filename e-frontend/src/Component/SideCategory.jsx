de8042f521a6391f693cf766ee1c691df1bf23b6
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-27 00:41:01 +0545

    Ecommerce

---
+++ e-frontend/src/Component/SideCategory.jsx
@@ -0,0 +1,57 @@
+import {React,useEffect,useState} from 'react';
+import logo from "../assets/images/logo.png"
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+
+
+const SideCategory = (props) => {
+ 
+   const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={()=>props.openSidePanel(false)}>
+      <div className="img-1 w-[100px] h-[30px] flex justify-center items-center    ">
+        <img src={logo} alt="Logo"  className="max-w-full max-h-full"/>
+      </div>
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+            
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+    
+  
+      <Drawer open={props.isOpen} onClose={()=>props.openSidePanel(true)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  )
+}
+
+export default SideCategory 
